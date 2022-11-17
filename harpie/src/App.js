cc4fc3e0c22189c25e0df1ec8007ff8f53ad9d27
Dytsaw <tomennana.samuel@outlook.fr>
2022-11-17 11:51:42 +0100

    refactoring react

--- harpie/src/App.js
+++ harpie/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import NosProjets from './Components/Nos_Projets/NosProjets';
 import NavBar from './Components/Main/1_NavBar/NavBar';
 import Footer from './Components/Main/8_Footer/Footer';
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      {/* <NosProjets/> */}
+      <NosProjets/>
       <Home/>
       <TakeAction/>
       <Projets/>
