ca43c36764d94eec605ddb47268ca6d7547e7a35
Dytsaw <tomennana.samuel@outlook.fr>
2022-11-21 14:02:02 +0100

    work only in responsive

--- harpie/src/Components/Main/1_NavBar/NavBar.js
+++ harpie/src/Components/Main/1_NavBar/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import logo from '../../../assets/logo_harpie.png'
 
 export default function NavBar() {
   return (
     <nav class="navbar navbar-expand-md navbar-light bg-custom">
                   <div class="container">
-                  <Link to="/"><a id="1" class="navbar-brand" href="#"  onClick={colorChangeLink} aria-current="page"><img src={logo} alt="logo harpie" width="55px"/></a></Link>
+                  <Link to="/"><a id="1" class="navbar-brand"   onClick={colorChangeLink} aria-current="page"><img src={logo} alt="logo harpie" width="55px"/></a></Link>
                     <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
                         aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -17,13 +17,13 @@ export default function NavBar() {
                             <Link to="nosProjets"><a id="2" class="nav-link"  onClick={colorChangeLink}>Nos projets</a></Link>
                             </li>
                             <li class="nav-item">
-                                <a id="3" class="nav-link" href="#" onClick={colorChangeLink}>Le saviez-vous</a>
+                                <a id="3" class="nav-link" onClick={colorChangeLink}>Le saviez-vous</a>
                             </li>
                             <li class="nav-item">
-                                <a id="4" class="nav-link"  href="#" onClick={colorChangeLink}>Nos histoires</a>
+                                <a id="4" class="nav-link" onClick={colorChangeLink}>Nos histoires</a>
                             </li>
                             <li class="nav-item">
-                                <a id="5" class="nav-link"  href="#" onClick={colorChangeLink}>Faites un don</a>
+                                <a id="5" class="nav-link" onClick={colorChangeLink}>Faites un don</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mon profil</a>
@@ -44,20 +44,20 @@ const lien2 = document.getElementById("2");
 const lien3 = document.getElementById("3");
 const lien4 = document.getElementById("4");
 const lien5 = document.getElementById("5");
-const activeLink = document.querySelector(".activey")
-const lienAll = lien1 + lien2 + lien3 + lien4 +lien5
+const activeLink = document.querySelectorAll(".activey")
+const lienAll = lien1 + lien2 + lien3 + lien4 + lien5;
 
-// const colorChangeLink = () => {!activeLink ? lienAll.classList.add("activey") : lienAll.classList.remove("activey")};
+const colorChangeLink = () => {!activeLink ? lienAll.classList.add("activey") : lienAll.classList.remove("activey")};
 
-function colorChangeLink(){
-    if(!activeLink){
-        lienAll.classList.add("activey")
-        console.log("true");
-    }else{
-        lienAll.classList.remove("activey")
-        console.log('false');
-    }
-}
+// function colorChangeLink(){
+//     if(!activeLink){
+//         lienAll.classList.add("activey")
+//         console.log("true");
+//     }else{
+//         lienAll.classList.remove("activey")
+//         console.log('false');
+//     }
+// }
 
 
 
